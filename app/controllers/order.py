85dd185eeefc72e2ee2f140d9d25cd84c49e9083
Lbayout <62385787+mendesbayout@users.noreply.github.com>
2023-01-24 22:16:23 -0300

    PIZ4: test coverage enhancement & CI testing fix. (#4)
    * feat: test coverage enhancement
    
    * feat: fixing flake issue
    
    * feat: fixing flake issues
    
    * feat: Fixing Flake8 warnings
    
    * feat: Fixing Flake8 warnings
    
    * feat: Fixing Flake8 warnings
    
    * feat: Implementing Workflow comment on external PR
    
    * feat: removing deprecated action
    
    * feat: improving test coverage to 100%

--- app/controllers/order.py
+++ app/controllers/order.py
@@ -1,8 +1,8 @@
 from sqlalchemy.exc import SQLAlchemyError
 
 from ..common.utils import check_required_keys
-from ..repositories.managers import (IngredientManager, OrderManager,
-                                     SizeManager)
+from ..repositories.managers import (BeverageManager, IngredientManager,
+                                     OrderManager, SizeManager)
 from .base import BaseController
 
 
@@ -11,8 +11,10 @@ class OrderController(BaseController):
     __required_info = ('client_name', 'client_dni', 'client_address', 'client_phone', 'size_id')
 
     @staticmethod
-    def calculate_order_price(size_price: float, ingredients: list):
-        price = sum(ingredient.price for ingredient in ingredients)
+    def calculate_order_price(size_price: float, ingredients: list, beverages: list) -> float:
+        ingredients_price = sum([ingredient.price for ingredient in ingredients])
+        beverages_price = sum([beverage.price for beverage in beverages])
+        price = size_price + ingredients_price + beverages_price
         return round(price, 2)
 
     @classmethod
@@ -28,10 +30,12 @@ class OrderController(BaseController):
             return 'Invalid size for Order', None
 
         ingredient_ids = current_order.pop('ingredients', [])
+        beverage_ids = current_order.pop('beverages', [])
         try:
             ingredients = IngredientManager.get_by_id_list(ingredient_ids)
-            price = cls.calculate_order_price(size.get('price'), ingredients)
+            beverages = BeverageManager.get_by_id_list(beverage_ids)
+            price = cls.calculate_order_price(size.get('price'), ingredients, beverages)
             order_with_price = {**current_order, 'total_price': price}
-            return cls.manager.create(order_with_price, ingredients), None
+            return cls.manager.create(order_with_price, ingredients, beverages), None
         except (SQLAlchemyError, RuntimeError) as ex:
             return None, str(ex)
